9b72c3c1131e42b2964109a3168ba00776909112
heloiseduval <heloise.duval@hotmail.fr>
2020-06-02 17:24:08 +0200

    update readme sql & uml

--- README.md
+++ README.md
@@ -10,11 +10,43 @@ Frais kilometrique
 Gestionnaire de stock
 Mise en relation avec expert comptable
 
-##contributeurs:
+##Contributeurs:
 
 Diana
 Ali
 Heloise
+
+##Dev:
+
+INSERTIONS SQL
+
+INSERT INTO credentials (pseudo, password) VALUES ('tutu', 'nhgbmn1434nse');
+
+INSERT INTO role (status) VALUES ('admin');
+
+INSERT INTO user (credentials_id, email, firstname, lastname, adress, role_id) VALUES (5,'tutu@gmail.fr', 'tutu', 'dupont','18 impasse des lauriers', 2);
+
+INSERT INTO userrole (user_id, role_id) VALUES (1,3);
+
+
+REQUETES SQL
+Affiche ceux qui ont 12 dans leur addresse:
+SELECT * FROM user WHERE address LIKE "12%";
+
+Double jointure sans union pour avoir info de user et role en passant par userrole:
+SELECT * FROM userrole 
+JOIN user ON user.id = userrole.user_id 
+JOIN role ON role.id = userrole.role_id;
+
+Outer join entre user et userrole:
+SELECT * FROM user 
+LEFT JOIN userrole ON user.id = userrole.user_id 
+UNION SELECT * FROM user 
+RIGHT JOIN userrole ON user.id = userrole.user_id;
+
+
+
+
 
 
 
--- capture_bills.sql
+++ capture_bills.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `amount`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `amount` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `tva` float NOT NULL,
   `ttc` float NOT NULL,
   `ht` float NOT NULL,
@@ -41,6 +41,32 @@ LOCK TABLES `amount` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `credentials`
+--
+
+DROP TABLE IF EXISTS `credentials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `credentials` (
+  `pseudo` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pseudo` (`pseudo`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `credentials`
+--
+
+LOCK TABLES `credentials` WRITE;
+/*!40000 ALTER TABLE `credentials` DISABLE KEYS */;
+INSERT INTO `credentials` VALUES ('toto','motdepasse',1),('tata','mbhrzfdnse',2),('tutu','nhgbmn1434nse',5),('titi','nhgbmn1434nse',6),('tete','passe',7);
+/*!40000 ALTER TABLE `credentials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `expense`
 --
 
@@ -48,8 +74,8 @@ DROP TABLE IF EXISTS `expense`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `expense` (
-  `id` int(11) NOT NULL,
-  `idAmount` int(6) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `amount_id` int(11) DEFAULT NULL,
   `location` varchar(250) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `purpose` text NOT NULL,
@@ -61,8 +87,8 @@ CREATE TABLE `expense` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_u` (`id`),
   KEY `id` (`id`),
-  KEY `idAmount` (`idAmount`),
-  CONSTRAINT `expense_ibfk_1` FOREIGN KEY (`idAmount`) REFERENCES `amount` (`id`)
+  KEY `amount_id` (`amount_id`),
+  CONSTRAINT `expense_ibfk_1` FOREIGN KEY (`amount_id`) REFERENCES `amount` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -76,6 +102,61 @@ LOCK TABLES `expense` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'admin'),(2,'manager');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `credentials_id` int(11) DEFAULT NULL,
+  `email` varchar(250) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `login` (`credentials_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`credentials_id`) REFERENCES `credentials` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,NULL,'toto@gmail.fr','toto','dupont','4 impasse des lauriers'),(2,2,'tata@gmail.fr','tata','dupont','8 impasse des lauriers'),(7,5,'tutu@gmail.fr','tutu','dupont','18 impasse des lauriers'),(8,6,'titi@gmail.fr','titi','dupont','12 impasse des lauriers'),(9,7,'tete@gmail.fr','tete','delariviere','12 impasse des lauriers');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `userexpense`
 --
 
@@ -86,7 +167,9 @@ CREATE TABLE `userexpense` (
   `user_id` int(11) DEFAULT NULL,
   `expense_id` int(11) DEFAULT NULL,
   KEY `expense_id` (`expense_id`),
-  CONSTRAINT `userexpense_ibfk_1` FOREIGN KEY (`expense_id`) REFERENCES `expense` (`id`)
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `userexpense_ibfk_1` FOREIGN KEY (`expense_id`) REFERENCES `expense` (`id`),
+  CONSTRAINT `userexpense_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -98,6 +181,33 @@ LOCK TABLES `userexpense` WRITE;
 /*!40000 ALTER TABLE `userexpense` DISABLE KEYS */;
 /*!40000 ALTER TABLE `userexpense` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `userrole`
+--
+
+DROP TABLE IF EXISTS `userrole`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userrole` (
+  `role_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `userrole_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `userrole_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userrole`
+--
+
+LOCK TABLES `userrole` WRITE;
+/*!40000 ALTER TABLE `userrole` DISABLE KEYS */;
+INSERT INTO `userrole` VALUES (2,7),(1,8),(2,9);
+/*!40000 ALTER TABLE `userrole` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -108,4 +218,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-01 18:55:24
+-- Dump completed on 2020-06-02 17:19:49
